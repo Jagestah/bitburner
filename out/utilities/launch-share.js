c5e2804a673966ccea29440bd2614a07aea9019c
JJ Mckeever <mckeeverjohnj@gmail.com>
2022-07-20 12:09:45 -0400

    Saving all this sweet work

--- out/utilities/launch-share.js
+++ out/utilities/launch-share.js
@@ -1,12 +1,33 @@
 /** @param {NS} ns */
+import { getRootHosts } from "./utilities/scanHosts"
 export async function main(ns, targetServer="home") {
 	ns.tail()
 	const scriptCost = 4.00
-	// const scriptHosts = await getRootHosts(ns)
-	const [totalRam, ramUsed] = ns.getServerRam(targetServer);
-	var maxThreads = Math.floor((totalRam-(ramUsed+64)) / scriptCost)
-	ns.printf("%s: %s", targetServer, maxThreads)
-	if (maxThreads > 0){
-		ns.run("share.js", maxThreads)
+	// ns.printf("%s: %s", targetServer, maxThreads)
+	var serverList = await getRootHosts(ns)
+	for (let index in serverList) {
+		var maxRam = ns.getServerMaxRam(serverList[index])
+		var usedRam = ns.getServerUsedRam(serverList[index])
+		// var [maxRam, ramUsed] = ns.getServerRam(serverList[index]);
+        if (!(serverList[index].includes("node"))){
+			if (serverList[index] != "home"){
+				var maxThreads = Math.floor((maxRam - usedRam) / scriptCost)
+				ns.printf("%s: %s", serverList[index], maxThreads)
+				if (maxThreads > 0){
+					if (maxThreads > 2000) {
+						var i = 1
+						var threadBatches = maxThreads / 1000
+						while (i < threadBatches) {
+							const initSleep = Math.floor(Math.random() * execTime)
+							ns.exec("share.js", serverList[index], Math.floor(maxThreads/threadBatches), targetServer, initSleep/2)
+							i++
+						}
+					} else {
+						ns.exec("share.js", serverList[index], maxThreads, serverList[index], 200)
+					}
+				}
+			}
+		}
 	}
 }
+3
