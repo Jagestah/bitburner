9da0b1c613204e6c4a3406012210ab99ec3d0450
JJ Mckeever <mckeeverjohnj@gmail.com>
2022-07-22 09:50:47 -0400

    Checkpoint for new bitnode

--- out/utilities/launch-share.js
+++ out/utilities/launch-share.js
@@ -9,25 +9,14 @@ export async function main(ns, targetServer="home") {
 		var maxRam = ns.getServerMaxRam(serverList[index])
 		var usedRam = ns.getServerUsedRam(serverList[index])
 		// var [maxRam, ramUsed] = ns.getServerRam(serverList[index]);
-        if (!(serverList[index].includes("node"))){
+        // if (!(serverList[index].includes("node"))){
 			if (serverList[index] != "home"){
 				var maxThreads = Math.floor((maxRam - usedRam) / scriptCost)
 				ns.printf("%s: %s", serverList[index], maxThreads)
 				if (maxThreads > 0){
-					if (maxThreads > 2000) {
-						var i = 1
-						var threadBatches = maxThreads / 1000
-						while (i < threadBatches) {
-							const initSleep = Math.floor(Math.random() * execTime)
-							ns.exec("share.js", serverList[index], Math.floor(maxThreads/threadBatches), targetServer, initSleep/2)
-							i++
-						}
-					} else {
-						ns.exec("share.js", serverList[index], maxThreads, serverList[index], 200)
-					}
+					ns.exec("share.js", serverList[index], Math.floor(maxThreads))
 				}
-			}
+			// }
 		}
 	}
 }
-3
